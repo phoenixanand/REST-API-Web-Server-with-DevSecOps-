23d90f8885faa574e5ea48a7169faac0760a70fd
phoenixanand <phoenixanand02@gmail.com>
2026-08-04 08:56:11 +0530

    kafka

--- app/database.py
+++ app/database.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@postgres:5432/testdb"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@postgres:5432/testdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal =  sessionmaker(autocommit=False, autoflush=False, bind=engine)
